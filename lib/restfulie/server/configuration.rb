a4f792af22f4c99b913b9bc1c24cb766de5d0a4f
guilherme silveira <guilherme.silveira@caelum.com.br>
2010-05-11 14:53:05 -0300

    using default port 3000

--- lib/restfulie/server/configuration.rb
+++ lib/restfulie/server/configuration.rb
@@ -1,8 +1,11 @@
 module Restfulie::Server::Configuration
   #Defines host to be passed to polymorphic_url.
   # You need to setup this to your own domain in order to generate meaningful links.
+  # If you want to configure the automatically generated links to point to something different
+  # than localhost:3000, change this configuration.
+  # You can also do it by using the setup method.
   mattr_accessor :host
-  @@host = 'localhost'
+  @@host = 'localhost:3000'
   
   # Passes a symbol to polymorphic_url in order to use a namespaced named_route.
   # So, if config.named_route_prefix = :rest, it will search for rest_album_url,
@@ -13,6 +16,10 @@ module Restfulie::Server::Configuration
   # This defines a Rails-like way to setup options. You can do, in a initializer:
   #   Restfulie::Server.setup do |config|
   #     config.host = 'mydomain.com'
+  #   end
+  # You can also set up a different port
+  #   Restfulie::Server.setup do |config|
+  #     config.host = 'mydomain.com:3000'
   #   end
   def self.setup
     yield self
--- spec/units/common/builder/marshalling/atom_spec.rb
+++ spec/units/common/builder/marshalling/atom_spec.rb
@@ -6,7 +6,7 @@ context "builder representations" do
   include Restfulie::Common::Builder::Helpers
   include ActionController::UrlWriter
   
-  default_url_options[:host] = 'localhost'
+  default_url_options[:host] = 'localhost:3000'
 
   context "marshalling atom" do
     context "for member" do
@@ -23,7 +23,7 @@ context "builder representations" do
       end
       
       it "set atom values from and generation" do
-        id      = "http://localhost/rest/albums/#{@album.id}"
+        id      = "http://localhost:3000/rest/albums/#{@album.id}"
         title   = "Album title"
         updated = DateTime.parse(DateTime.now.to_s)
       
@@ -93,14 +93,14 @@ context "builder representations" do
             member.links.delete(:songs)
             member.links << link(:songs)
             
-            member.links << link(:rel => :artist, :href => "http://localhost/albums/1/artist", :type => "application/atom+xml")
-            member.links << link(:rel => 'lyrics', :href => "http://localhost/albums/1/lyrics", :type => "application/atom+xml")
+            member.links << link(:rel => :artist, :href => "http://localhost:3000/albums/1/artist", :type => "application/atom+xml")
+            member.links << link(:rel => 'lyrics', :href => "http://localhost:3000/albums/1/lyrics", :type => "application/atom+xml")
           end
           
           entry = Atom::Entry.load_entry(builder.to_atom)
           entry.links.should be_include(create_atom_link('songs' , album_songs_url(@album)))
-          entry.links.should be_include(create_atom_link('artist', "http://localhost/albums/1/artist"))
-          entry.links.should be_include(create_atom_link('lyrics', "http://localhost/albums/1/lyrics"))
+          entry.links.should be_include(create_atom_link('artist', "http://localhost:3000/albums/1/artist"))
+          entry.links.should be_include(create_atom_link('lyrics', "http://localhost:3000/albums/1/lyrics"))
         end
       end
       
@@ -113,9 +113,9 @@ context "builder representations" do
       end
       
       it "infer atom values from basic collection describe and generation" do
-        builder = describe_collection(@albums, :values => { :id => "http://localhost/albums" })
+        builder = describe_collection(@albums, :values => { :id => "http://localhost:3000/albums" })
         feed    = Atom::Feed.load_feed(builder.to_atom)
-        feed.id.should == "http://localhost/albums"
+        feed.id.should == "http://localhost:3000/albums"
         feed.title.should == "Albums feed"
         feed.entries.size.should == @albums.size
         feed.entries.first.title == @albums.first.title
@@ -132,8 +132,8 @@ context "builder representations" do
       end
       
       it "with namespace" do
-        builder = describe_collection(@albums, :values => { :id => "http://localhost/albums" }) do |collection|
-          collection.namespace(:albums, "http://localhost/albums") do |ns|
+        builder = describe_collection(@albums, :values => { :id => "http://localhost:3000/albums" }) do |collection|
+          collection.namespace(:albums, "http://localhost:3000/albums") do |ns|
             ns.count = 10
           end
         end
@@ -143,12 +143,12 @@ context "builder representations" do
       end
       
       it "customize members" do
-        builder = describe_collection(@albums, :values => { :id => "http://localhost/albums" }) do |collection|
+        builder = describe_collection(@albums, :values => { :id => "http://localhost:3000/albums" }) do |collection|
           collection.describe_members(:namespace => "http://example.com/albums") do |member, album|
             member.links << link(:self)
-            member.links << link(:rel => :artists, :href => "http://localhost/albums/#{album.id}/artists", :type => "application/atom+xml")
+            member.links << link(:rel => :artists, :href => "http://localhost:3000/albums/#{album.id}/artists", :type => "application/atom+xml")
     
-            member.namespace(:music, "http://localhost/musics") do |ns|
+            member.namespace(:music, "http://localhost:3000/musics") do |ns|
               ns.count = 3
             end
           end
@@ -158,7 +158,7 @@ context "builder representations" do
         album = @albums.first
         entry = feed.entries.first
         entry.links.should be_include(create_atom_link('self', album_url(album)))
-        entry.links.should be_include(create_atom_link('artists', "http://localhost/albums/#{album.id}/artists"))
+        entry.links.should be_include(create_atom_link('artists', "http://localhost:3000/albums/#{album.id}/artists"))
         entry.albums_length == album.length
         entry.albums_description == album.description
         entry.music_count.to_i.should == 3
@@ -166,21 +166,21 @@ context "builder representations" do
 
       it "generate full representation" do
         options = {
-          :self => "http://localhost/albums",
-          :values => { :id => "http://localhost/albums" }
+          :self => "http://localhost:3000/albums",
+          :values => { :id => "http://localhost:3000/albums" }
         }
         
         builder = describe_collection(@albums, options) do |collection|
-          collection.links << link(:rel => :next, :href => "http://localhost/albums/next")
+          collection.links << link(:rel => :next, :href => "http://localhost:3000/albums/next")
       
-          collection.namespace(:albums, "http://localhost/albums") do |ns|
+          collection.namespace(:albums, "http://localhost:3000/albums") do |ns|
             ns.count = 10
           end
       
           collection.describe_members(:namespace => "http://example.com/albums") do |member, album|
-            member.links << link(:rel => :artists, :href => "http://localhost/albums/#{album.id}/artists", :type => "application/atom+xml")
+            member.links << link(:rel => :artists, :href => "http://localhost:3000/albums/#{album.id}/artists", :type => "application/atom+xml")
       
-            member.namespace(:music, "http://localhost/musics") do |ns|
+            member.namespace(:music, "http://localhost:3000/musics") do |ns|
               ns.count = 3
             end
           end
